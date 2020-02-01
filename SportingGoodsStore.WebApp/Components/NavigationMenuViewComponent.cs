3294c33b9d7747908ee6fa66b18574e70ec95772
ALEXEY SHPAVDA <alexeyshpavda@gmail.com>
2020-02-01 10:36:53 +0300

    Show all categories in NavigationMenu

--- SportingGoodsStore.WebApp/Components/NavigationMenuViewComponent.cs
+++ SportingGoodsStore.WebApp/Components/NavigationMenuViewComponent.cs
@@ -1,12 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SportingGoodsStore.WebApp.Models.Interfaces;
+using System.Linq;
 
 namespace SportingGoodsStore.WebApp.Components
 {
     public class NavigationMenuViewComponent : ViewComponent
     {
-        public string Invoke()
+        private IProductRepository repository;
+        public NavigationMenuViewComponent(IProductRepository repo)
         {
-            return "Hello from the Nav View Component";
+            repository = repo;
+        }
+        public IViewComponentResult Invoke()
+        {
+            return View(repository.Products
+                .Select(x => x.Category)
+                .Distinct()
+                .OrderBy(x => x));
         }
     }
 }
