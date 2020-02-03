2bd3b9e4d622380a3bbda3de4fe3b08a55151e47
ALEXEY SHPAVDA <alexeyshpavda@gmail.com>
2020-02-03 11:34:03 +0300

    Add Index View for Cart Controller

--- SportingGoodsStore.WebApp/Controllers/CartController.cs
+++ SportingGoodsStore.WebApp/Controllers/CartController.cs
@@ -3,17 +3,28 @@ using Microsoft.AspNetCore.Mvc;
 using SportingGoodsStore.WebApp.Models;
 using SportingGoodsStore.WebApp.Models.Interfaces;
 using Microsoft.AspNetCore.Http;
-using Newtonsoft.Json;
+using SportingGoodsStore.WebApp.Infrastructure;
 
 namespace SportingGoodsStore.WebApp.Controllers
 {
     public class CartController : Controller
     {
         private IProductRepository repository;
+
         public CartController(IProductRepository repo)
         {
             repository = repo;
         }
+
+        public ViewResult Index(string returnUrl)
+        {
+            return View(new CartIndexViewModel
+            {
+                Cart = GetCart(),
+                ReturnUrl = returnUrl
+            });
+        }
+
         public RedirectToActionResult AddToCart(int productId, string returnUrl)
         {
             var product = repository.Products.FirstOrDefault(p => p.ProductID == productId);
@@ -30,8 +41,8 @@ namespace SportingGoodsStore.WebApp.Controllers
 
         public RedirectToActionResult RemoveFromCart(int productId, string returnUrl)
         {
-            Product product = repository.Products
-            .FirstOrDefault(p => p.ProductID == productId);
+            Product product = repository.Products.FirstOrDefault(p => p.ProductID == productId);
+
             if (product != null)
             {
                 Cart cart = GetCart();
@@ -43,19 +54,11 @@ namespace SportingGoodsStore.WebApp.Controllers
 
         private Cart GetCart()
         {
-            var data = HttpContext.Session.GetString("Cart");
-
-            if (data == null)
-            {
-                return new Cart();
-            }
-
-            return JsonConvert.DeserializeObject<Cart>(data);
+            return HttpContext.Session.GetJson<Cart>("Cart") ?? new Cart();
         }
-
         private void SaveCart(Cart cart)
         {
-            HttpContext.Session.SetString("Cart", JsonConvert.SerializeObject(cart));
+            HttpContext.Session.SetJson("Cart", cart);
         }
     }
 }
