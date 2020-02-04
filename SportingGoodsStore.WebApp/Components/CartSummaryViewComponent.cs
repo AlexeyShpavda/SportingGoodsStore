6d577f6747bd7a12a866596fa4ce2aaa5450ceff
ALEXEY SHPAVDA <alexeyshpavda@gmail.com>
2020-02-04 09:50:56 +0300

    Add new components on UI

---
+++ SportingGoodsStore.WebApp/Components/CartSummaryViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportingGoodsStore.WebApp.Models;
+
+namespace SportingGoodsStore.WebApp.Components
+{
+    public class CartSummaryViewComponent : ViewComponent
+    {
+        private Cart cart;
+
+        public CartSummaryViewComponent(Cart cartService)
+        {
+            cart = cartService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View(cart);
+        }
+    }
+}
---
+++ SportingGoodsStore.WebApp/Controllers/OrderController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportingGoodsStore.WebApp.Models;
+
+namespace SportingGoodsStore.WebApp.Controllers
+{
+    public class OrderController : Controller
+    {
+        public ViewResult Checkout() => View(new Order());
+    }
+}
