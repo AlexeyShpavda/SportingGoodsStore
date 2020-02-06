e21281bca2837c971d945691ad648150a50b6725
ALEXEY SHPAVDA <alexeyshpavda@gmail.com>
2020-02-06 08:08:08 +0300

    Update Startup file

--- SportingGoodsStore.WebApp/Startup.cs
+++ SportingGoodsStore.WebApp/Startup.cs
@@ -40,10 +40,11 @@ namespace SportingGoodsStore.WebApp
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseStatusCodePages();
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
